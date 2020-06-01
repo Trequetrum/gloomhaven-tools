372b6942b141af29003da9566e7e7e9aaad4932d
Trequetrum <mrk.c.fischer@gmail.com>
2020-06-01 18:00:59 -0400

    Angular Materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,38 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PublicToolsMenuComponent } from './public-tools-menu/public-tools-menu.component';
 import { AccountToolsMenuComponent } from './account-tools-menu/account-tools-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { GloomNavComponent } from './gloom-nav/gloom-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PublicToolsMenuComponent,
-    AccountToolsMenuComponent
+    AccountToolsMenuComponent,
+    GloomNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
