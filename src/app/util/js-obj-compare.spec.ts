3d9a0cd6aa3678e5ffb6bd51db845d288a4148ce
Trequetrum <trequetrum@gmail.com>
2020-09-30 15:53:05 -0400

    Remove object compare util functions

--- src/app/util/js-obj-compare.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JsObjCompare } from './js-obj-compare';
-
-describe('JsObjCompare', () => {
-  it('should create an instance', () => {
-    expect(new JsObjCompare()).toBeTruthy();
-  });
-});
